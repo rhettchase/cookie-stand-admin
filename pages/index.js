d08fe7d67efb18fcf4fd666a912de70afd3516e9
RHETT CHASE <rhettjchase@gmail.com>
2024-02-29 17:25:33 -0800

    adds auth provider

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import CookieStandAdmin from "@/components/CookieStandAdmin";
+import LoginForm from "@/components/LoginForm";
+import { useAuth } from '../contexts/auth';
 
 export default function Home() {
+
+  const { user, login } = useAuth();
   
   return (
-    <div>
-      <CookieStandAdmin />
+    <div className="p-4">
+      {user ?
+                <>
+                    <h1>Logged in: {user.email}</h1>
+                    <CookieStandAdmin />
+                </>
+                :
+                <LoginForm onLogin={login} />
+            }
     </div>
   );
 }
